24824f4233c038a54e3dc8394056889db4292a43
urtesala <urtesaladziute@gmail.com>
2022-11-05 23:40:35 +0300

    generate password function all done

--- js/main.js
+++ js/main.js
@@ -8,35 +8,86 @@ const uppercaseEl = document.getElementById("uppercase");
 const lowercaseEl = document.getElementById("lowercase");
 const numbersEl = document.getElementById("numbers");
 const symbolsEl = document.getElementById("symbols");
+const generateEl = document.getElementById("generate");
 const clipboardEl = document.getElementById("clipboard");
 
-const randomFunction = {
+const randomFunc = {
   lower: getRandomLower,
   upper: getRandomUpper,
   number: getRandomNumber,
   symbol: getRandomSymbol,
 };
 
+// Event listeners
+
+generateEl.addEventListener("click", () => {
+  const length = +lengthEl.value;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbol = symbolsEl.checked;
+
+  resultEl.innerText = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length
+  );
+});
+
+// Generate password function
+
+function generatePassword(lower, upper, number, symbol, length) {
+  // 1. Init password variables
+  // 2. Filter out unchecked types
+  // 3. Loop over length and call generator function for each type
+  // 4. Add final password to the passw variable and return it
+
+  let generatedPassword = "";
+
+  const typesCount = lower + upper + number + symbol;
+  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+    (item) => Object.values(item)[0]
+  );
+  //   console.log("typesArr ===", typesArr);
+
+  if (typesCount === 0) {
+    return "";
+  }
+
+  // create a loop
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach((type) => {
+      const funcName = Object.keys(type)[0];
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+
+  const finalPassword = generatedPassword.slice(0, length);
+
+  return finalPassword;
+}
 // Generator functions
 // https://www.net-comber.com/charset.html
 
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
-console.log("getRandomLower() ===", getRandomLower());
+//console.log("getRandomLower() ===", getRandomLower());
 
 function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
-console.log("getRandomUpper() ===", getRandomUpper());
+//console.log("getRandomUpper() ===", getRandomUpper());
 
 function getRandomNumber() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
-console.log("getRandomNumber() ===", getRandomNumber());
+//console.log("getRandomNumber() ===", getRandomNumber());
 
 function getRandomSymbol() {
   const symbols = "!@#$%^&*(){}[]=<>,./";
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
-console.log("getRandomSymbol() ===", getRandomSymbol());
+//console.log("getRandomSymbol() ===", getRandomSymbol());
