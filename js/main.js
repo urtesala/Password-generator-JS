82a76bb8ef43ef34cfa4a6b9dd4d65abd492e93b
urtesala <urtesaladziute@gmail.com>
2022-11-05 22:35:11 +0300

    generator function, getting DOM elements

--- js/main.js
+++ js/main.js
@@ -1,2 +1,42 @@
-'use strict';
-console.log('main.js');
+"use strict";
+console.log("main.js");
+
+// DOM elements
+const resultEl = document.getElementById("result");
+const lengthEl = document.getElementById("length");
+const uppercaseEl = document.getElementById("uppercase");
+const lowercaseEl = document.getElementById("lowercase");
+const numbersEl = document.getElementById("numbers");
+const symbolsEl = document.getElementById("symbols");
+const clipboardEl = document.getElementById("clipboard");
+
+const randomFunction = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+};
+
+// Generator functions
+// https://www.net-comber.com/charset.html
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+console.log("getRandomLower() ===", getRandomLower());
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+console.log("getRandomUpper() ===", getRandomUpper());
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+console.log("getRandomNumber() ===", getRandomNumber());
+
+function getRandomSymbol() {
+  const symbols = "!@#$%^&*(){}[]=<>,./";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+console.log("getRandomSymbol() ===", getRandomSymbol());
